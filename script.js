45c54507364d7415dcd9de84560e8ffb419f49b7
swesslen <simon@jolowe.se>
2025-04-23 12:51:18 +0200

    delete button functional
    + styling

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function createList (array) {
     let listDiv = document.querySelector("#divList")
     listDiv.innerHTML = "";
-    for (let city of cities) {
+    for (let city of array) {
         let cityDiv = document.createElement("div");
         cityDiv.classList.add("cityDiv");
         listDiv.appendChild(cityDiv);
@@ -9,13 +9,17 @@ function createList (array) {
 
         const deleteButton = document.createElement("button");
         deleteButton.classList.add("deleteButton");
+        deleteButton.id = `${city.name}`;
         deleteButton.innerHTML = `Delete`;
         cityDiv.appendChild(deleteButton);
-
         deleteButton.addEventListener("click", function () {
+            let indexToDelete = cities.findIndex(x => x.name == deleteButton.id);
+            console.log(indexToDelete);
             
+            cities.splice(indexToDelete, 1);
+            createList(cities);
         })
     }
 }
-
 createList(cities);
+
